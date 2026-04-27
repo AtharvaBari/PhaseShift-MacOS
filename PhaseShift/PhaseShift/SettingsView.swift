ad338af2141fe919d65c01888645efbea2d6fde3
Atharva Bari <atharvabari0708@gmail.com>
2026-04-27 22:17:45 +0530

    v2.0.4 - test

--- PhaseShift/PhaseShift/SettingsView.swift
+++ PhaseShift/PhaseShift/SettingsView.swift
@@ -77,7 +77,7 @@ struct SettingsView: View {
             }
             .padding()
             
-            Text("PhaseShift v2.0.3")
+            Text("PhaseShift v2.0.4")
                 .font(.caption2)
                 .foregroundColor(.secondary)
                 .padding(.bottom, 8)
